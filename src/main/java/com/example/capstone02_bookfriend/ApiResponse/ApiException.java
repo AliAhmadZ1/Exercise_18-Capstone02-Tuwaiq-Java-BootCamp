ecf6baa7ddb5c828924e52fbd6c85e61a7cd340b
aliali <aliali@test.com>
2025-04-16 03:04:50 +0300

    Api Exception class to throw messages

---
+++ src/main/java/com/example/capstone02_bookfriend/ApiResponse/ApiException.java
@@ -0,0 +1,9 @@
+package com.example.capstone02_bookfriend.ApiResponse;
+
+public class ApiException extends RuntimeException{
+
+    public ApiException(String message){
+        super(message);
+    }
+
+}
